a2db563941b1094586010bc26a6490f14bb49a2f
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-25 19:36:39 +0100

    pierwszy commit

--- src/main/java/pl/javastart/voting/VotingResult.java
+++ src/main/java/pl/javastart/voting/VotingResult.java
@@ -1,9 +1,25 @@
 package pl.javastart.voting;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Możesz dodać kolejne metody i pola do klasy. Nie zmieniaj istniejących metod.
  */
 public class VotingResult {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    private List<Vote> votes = new ArrayList<>();
+
+    public List<Vote> getVotes() {
+        return votes;
+    }
+
+    public void setVotes(List<Vote> votes) {
+        this.votes = votes;
+    }
 
     /**
      * Metoda powinna drukować wyniki głosowania w takiej postaci:
@@ -12,7 +28,33 @@ public class VotingResult {
      * Wstrzymało się: 8.47%
      */
     public void printResults() {
-        // metoda powinna drukować wyniki głosowania
+        int forVotesCounter = 0;
+        int againstVotesCounter = 0;
+        int refrainCounter = 0;
+        double allVotesCounter = 0;
+        for (Vote vote : votes) {
+            try {
+                if (vote.getVote() == true) {
+                    forVotesCounter++;
+                    allVotesCounter++;
+                } else if (vote.getVote() == false) {
+                    againstVotesCounter++;
+                    allVotesCounter++;
+                } else if (vote.getVote() == null) {
+                    continue;
+                }
+            } catch (NullPointerException e) {
+                refrainCounter++;
+                allVotesCounter++;
+            }
+        }
+        double percentFor = forVotesCounter / allVotesCounter * 100;
+        double percentAgainst = againstVotesCounter / allVotesCounter * 100;
+        double percentRefrain = refrainCounter / allVotesCounter * 100;
+        System.out.println("Głosów za: " + df.format(percentFor) + "%");
+        System.out.println("Głosów przeciw: " + df.format(percentAgainst) + "%");
+        System.out.println("Wstrzymało się: " + df.format(percentRefrain) + "%");
+
     }
 
     /**
@@ -23,5 +65,20 @@ public class VotingResult {
      */
     public void printVoteForVoter(String voterName) {
 
+        for (Vote vote : votes) {
+            if (voterName.equals(vote.getVoter())) {
+                try {
+                    if (vote.getVote() == true) {
+                        System.out.println(vote.getVoter() + ": ZA");
+                    } else if (vote.getVote() == false) {
+                        System.out.println(vote.getVoter() + ": PRZECIW");
+                    } else {
+                        continue;
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println(vote.getVoter() + ": WSTRZYMAŁ SIĘ");
+                }
+            }
+        }
     }
 }
